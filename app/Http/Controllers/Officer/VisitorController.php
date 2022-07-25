f7aaa28a2c38f1713569374e1c961a7558bce25f
xcode17 <do.crazy192@gmail.com>
2022-07-25 20:41:52 +0700

    fix datatables

--- app/Http/Controllers/Officer/VisitorController.php
+++ app/Http/Controllers/Officer/VisitorController.php
@@ -29,12 +29,7 @@ class VisitorController extends Controller
 						->addColumn('criminal', function ($item) {
 							return $item->criminal->name;
 						})
-						->addColumn('action', function($row){
-								$actionBtn = '<a href="javascript:void(0)" class="edit btn btn-success btn-sm">Edit</a> <a href="javascript:void(0)" class="delete btn btn-danger btn-sm">Delete</a>';
-								return $actionBtn;
-						})
-						->rawColumns(['action'])
-						->make(true);
+							->make(true);
 			}
 		}
 
